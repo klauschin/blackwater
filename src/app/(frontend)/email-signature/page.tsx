957361c63d99f4e7ddea42829c3cc6d75536889e
Klaus Chin <chinssiang@gmail.com>
2026-01-04 21:28:03 +0800

    fix: update SITE_URL fallback and adjust import path for routes types

--- src/app/(frontend)/email-signature/page.tsx
+++ src/app/(frontend)/email-signature/page.tsx
@@ -1,6 +1,6 @@
 import { PageEmailSignature } from './_components/PageEmailSignature';
 
 export default async function Page() {
-	const SITE_URL = process.env.SITE_URL;
+	const SITE_URL = process.env.SITE_URL || '';
 	return <PageEmailSignature siteUrl={SITE_URL} />;
 }
